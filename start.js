d767c91cbc59b7bf7fbf5dbd12443aee1422b22c
VLadYxa343 <babinovvlad@gmail.com>
2019-09-19 23:37:05 +0500

    123

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 app.listen(3000);
@@ -25,66 +27,90 @@ function getBalance(token) {
         });
     })
 }
+
 function getStatus(token) {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         let headers_to_send = {
             Authorization: 'Basic ' + token,
             Company: 'Verny',
         };
-        request({uri: 'https://rsloyalty.verno-info.ru/RS.Loyalty.WebMobileService/api/discountCard/Get', encoding: 'UTF-8', headers: headers_to_send}, (e, r, p) => {
-            if(p === 'Ошибка клиентской авторизации.') resolve(null);
+        request({
+            uri: 'https://rsloyalty.verno-info.ru/RS.Loyalty.WebMobileService/api/discountCard/Get',
+            encoding: 'UTF-8',
+            headers: headers_to_send
+        }, (e, r, p) => {
+            if (p === 'Ошибка клиентской авторизации.') resolve(null);
             let page = JSON.parse(p);
             resolve(page)
         })
     })
 }
-function getQR(token, number){
+
+function getQR(token, number) {
     return new Promise(((resolve, reject) => {
         let headers_to_send = {
             Authorization: 'Basic ' + token,
             Company: 'Verny',
         };
-        request({uri: 'https://rsloyalty.verno-info.ru/RS.Loyalty.WebMobileService/api/DiscountCard/OneTimeCode?number=' + number, encoding: 'UTF-8', headers: headers_to_send}, (e, r, p) =>{
-            if(p === 'Ошибка клиентской авторизации.') resolve(null);
+        request({
+            uri: 'https://rsloyalty.verno-info.ru/RS.Loyalty.WebMobileService/api/DiscountCard/OneTimeCode?number=' + number,
+            encoding: 'UTF-8',
+            headers: headers_to_send
+        }, (e, r, p) => {
+            if (p === 'Ошибка клиентской авторизации.') resolve(null);
 
             let x = JSON.parse(p);
             resolve(x['Code']);
         })
     }))
 }
-function verify(token){
+
+function verify(token) {
     return new Promise(((resolve, reject) => {
         let headers_to_send = {
             Authorization: 'Basic ' + token,
             Company: 'Verny',
         };
-        request({uri: 'https://rsloyalty.verno-info.ru/RS.Loyalty.WebMobileService/api/discountCard/Get', encoding: 'UTF-8', headers: headers_to_send}, (e, r, p) => {
-            if(p === "Ошибка клиентской авторизации.") resolve(r.statusCode);
-            else resolve(r.statusCode)
+        request({
+            uri: 'https://rsloyalty.verno-info.ru/RS.Loyalty.WebMobileService/api/discountCard/Get',
+            encoding: 'UTF-8',
+            headers: headers_to_send
+        }, (e, r, p) => {
+            if(e) resolve(e);
+            else resolve(r.statusCode);
         })
     }))
 }
-
 app.get('/', (req, res) => {
-    async function f(token) {
-        amount = await getBalance(token);
-
-        status = await getStatus(token);
-
-        qr = await getQR(token, status[0]['Number']);
-        qr = qr.replace(/\+/g, "%2B");
-        res.render('scratch', {amount: amount, number: status[0]['Number'], canpay: status[0]['CanPayBonuses'], qr: qr});
-        console.log(qr)
-    }
-    f('f842f38d-5cb2-4454-9486-71809e143863');
+    res.sendFile(__dirname + '/views/index.html')
 });
-app.get('/tokens/:token', (req, res)=>{
-    async function f1() {
-        let x = await verify(req.params.token);
-        if (x !== 200)
-            res.send('err');
-        else
-            res.send('ok')
+app.post('/getqr', urlencodedParser, (req, res) => {
+    if(!req.body || !req.body['token']){
+        res.send('Ошибка')
     }
-    f1()
+    else{
+        async function f(token) {
+            if(await verify(token) === 200) {
+                amount = await getBalance(token);
+
+                status = await getStatus(token);
+
+                qr = await getQR(token, status[0]['Number']);
+                qr = qr.replace(/\+/g, "%2B");
+                res.render('scratch', {
+                    amount: amount,
+                    number: status[0]['Number'],
+                    canpay: status[0]['CanPayBonuses'],
+                    qr: qr
+                });
+                console.log(qr)
+            }
+            else{
+                res.send('Ошибка авторизации')
+            }
+        }
+        f(req.body['token']);
+
+    }
+
 });
