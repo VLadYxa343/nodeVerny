ce9a33a968ea46ea3a78c7618e39b1dc8f0635e8
VLadYxa343 <babinovvlad@gmail.com>
2019-09-21 18:31:03 +0500

    123

--- start.js
+++ start.js
@@ -2,11 +2,12 @@ const request = require('request');
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 app.listen(3000);
 var amount, status, qr;
+
 
 function getBalance(token) {
     return new Promise((resolve, reject) => {
@@ -76,39 +77,47 @@ function verify(token) {
             encoding: 'UTF-8',
             headers: headers_to_send
         }, (e, r, p) => {
+            console.log(p);
             if(e) resolve(e);
             else resolve(r.statusCode);
         })
     }))
 }
+
+function registation(token){
+
+}
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 });
+
 app.post('/getqr', urlencodedParser, (req, res) => {
-    if(!req.body || !req.body['token']){
+    if (!req.body || !req.body['token']) {
         res.send('Ошибка')
-    }
-    else{
+    } else {
         async function f(token) {
-            if(await verify(token) === 200) {
+            if (await verify(token) === 200) {
                 amount = await getBalance(token);
 
                 status = await getStatus(token);
-
-                qr = await getQR(token, status[0]['Number']);
-                qr = qr.replace(/\+/g, "%2B");
-                res.render('scratch', {
-                    amount: amount,
-                    number: status[0]['Number'],
-                    canpay: status[0]['CanPayBonuses'],
-                    qr: qr
-                });
-                console.log(qr)
-            }
-            else{
+                if(status[0]) {
+                    qr = await getQR(token, status[0]['Number']);
+                    qr = qr.replace(/\+/g, "%2B");
+                    res.render('scratch', {
+                        amount: amount,
+                        number: status[0]['Number'],
+                        canpay: status[0]['CanPayBonuses'],
+                        qr: qr
+                    });
+                    console.log(qr)
+                }
+                else res.send('Карты не существует')
+            } else {
                 res.send('Ошибка авторизации')
             }
         }
+
         f(req.body['token']);
 
     }
